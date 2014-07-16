99683a66f5c00699fac1d7c456f3b08c8242217c
Vladislav Skurishin <vladthelittleone@gmail.com>
2014-07-17 01:46:15 +0400

    [#13] Add custom titles.

--- sprinkle-web/src/main/webapp/resources/js/sprinkle/angular/utils/map.js
+++ sprinkle-web/src/main/webapp/resources/js/sprinkle/angular/utils/map.js
@@ -19,13 +19,13 @@ function SprinkleMap() {
         maxZoom: 19,
         minZoom: 2
     })
-    .setView([40, -74.50], 9);
+        .setView([40, -74.50], 9);
 
     // Create feature for markers
     var featureLayer = L.mapbox.featureLayer().addTo(map);
 
     // Centring
-    featureLayer.on('click', function(e) {
+    featureLayer.on('click', function (e) {
         map.panTo(e.layer.getLatLng());
         var marker = e.layer;
         var feature = marker.feature;
@@ -47,7 +47,7 @@ function SprinkleMap() {
         // All fields of root scope available for all child scopes.
         var rootScope = angular.element(document).
             injector().invoke(
-            function($rootScope){
+            function ($rootScope) {
                 return $rootScope;
             });
 
@@ -62,15 +62,36 @@ function SprinkleMap() {
     /**
      * Show a markers tooltip on mouse over instead of on click.
      */
-    featureLayer.on('mouseover', function(e) {
-//        e.layer.openPopup();
+    featureLayer.on('mouseover', function (e) {
+        e.layer.openPopup();
     });
 
     /**
      * Hide a markers tooltip on mouse out.
      */
-    featureLayer.on('mouseout', function(e) {
-//        e.layer.closePopup();
+    featureLayer.on('mouseout', function (e) {
+        e.layer.closePopup();
+    });
+
+    // Add custom popups to each using our custom feature properties
+    featureLayer.on('layeradd', function (e) {
+        var marker = e.layer,
+            feature = marker.feature;
+
+        console.log (feature.properties.title);
+        // Create custom popup content
+        var popupContent =
+            '<div class="markerPopup">' +
+                '<img src="https://i.imgur.com/exemdwr.png"/>' +
+                '<div class="markerTitle" style="background: ' + feature.properties['marker-color']+ '">'
+                    + feature.properties.title + '</div>' +
+            '</div>';
+
+        // http://leafletjs.com/reference.html#popup
+        marker.bindPopup(popupContent, {
+            closeButton: false,
+            minWidth: 250
+        });
     });
 
     this.getMap = function () {
@@ -78,7 +99,7 @@ function SprinkleMap() {
     };
 
     // Latitude and Longitude
-    this.setMarker = function (url, lng, lat, title, description, size, type){
+    this.setMarker = function (url, lng, lat, title, description, size, type) {
         $.ajax({
             type: "POST",
             async: false,
